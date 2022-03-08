a3dad957692fac0bdefb2f39b5e0e112c1451c4d
h-albert-lee <gksdnf424@korea.ac.kr>
2022-03-08 16:28:58 +0900

    initial commit

---
+++ 섹션 2/1. k번째 약수/AA.py
@@ -0,0 +1,15 @@
+
+
+n, k= map(int, input().split())
+
+answer_list = []
+
+for i in range(1,n+1):
+    if n % i == 0:
+        answer_list.append(i)
+try:
+    answer = answer_list[k-1]
+except:
+    answer = -1
+
+print(answer)
