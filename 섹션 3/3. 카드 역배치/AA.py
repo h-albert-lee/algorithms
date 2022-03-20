80fd9ebde73dc739be48518b427c628d79ae2a0f
h-albert-lee <gksdnf424@korea.ac.kr>
2022-03-21 08:57:52 +0900

    solved(sol)

--- 섹션 3/3. 카드 역배치/AA.py
+++ 섹션 3/3. 카드 역배치/AA.py
@@ -1,21 +1,14 @@
 #카드 역배치
-#unsolved
+#solved
 
 init = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-check_number = 0
 
-n, k = map(int, input().split())
+for _ in range(10):
+    n, k = map(int, input().split())
+    iternum = (k-n+1)//2
+    for each in range(iternum):
+        init[n+each], init[k-each] = init[k-each], init[n+each]
 
-
-init_list = init[n:k+1]
-change_list = [0]*len(init_list)
-for i in range(len(init_list)):
-    init_list_num = len(init_list) -1 - i
-    change_list[i] = init_list[init_list_num]
-
-init[n:k+1] = change_list
-check_number = check_number +1
-
-if check_number == 10:
-    for i in range(1,21):
-        print(init[i], end = ' ')
+init.pop(0)
+for i in init:
+    print(i, end = ' ')
