1718303cf9b70c10dea21df4df95ebfd4874a76f
h-albert-lee <gksdnf424@korea.ac.kr>
2022-03-22 09:14:42 +0900

    solved(sol)

--- 섹션 3/4. 두 리스트 합치기/AA.py
+++ 섹션 3/4. 두 리스트 합치기/AA.py
@@ -1,12 +1,33 @@
 #두 리스트 합치기
 #time limit exceed
 
-lists = []
-for _ in range(4):
-    length = int(input())
-    lists.append(map(int, input().split()))
+n = int(input())
+N = list(map(int, input().split()))
+m = int(input())
+M = list(map(int, input().split()))
+#n = 3
+#N = [1,3,5]
+#m = 5
+#M = [2,3,6,7,9]
 
-lists.sort()
+#pointer
+p1 = 0
+p2 = 0
+ans_list = []
 
-for i in lists:
+while p1<n and p2<m:
+    if N[p1] <= M[p2]:
+        ans_list.append(N[p1])
+        p1 += 1
+    elif N[p1] > M[p2]:
+        ans_list.append(M[p2])
+        p2 += 1
+
+if p1<n:
+    ans_list = ans_list + N[p1:]
+
+if p2<m:
+    ans_list = ans_list + M[p2:]
+
+for i in ans_list:
     print(i, end = ' ')
