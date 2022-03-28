0750a1032d49c3865275679fe9ee557cf27caf5f
h-albert-lee <gksdnf424@korea.ac.kr>
2022-03-28 11:01:33 +0900

    solved(sol)

--- 섹션 3/7. 사과나무/AA.py
+++ 섹션 3/7. 사과나무/AA.py
@@ -1,34 +1,29 @@
 #사과나무(다이아몬드)
-#unsolved
+#solved(sol)
 
-#n=int(input())
-#A=[list(map(int, input().split())) for _ in range(n)]
-
+n=int(input())
+A=[list(map(int, input().split())) for _ in range(n)]
+'''
 n = 5
 A = [[10,13,10,12,15],
     [12,39,30,23,11],
     [11,25,50,53,15],
     [19,27,29,37,27],
     [19,13,30,13,19]]
-
-center = n//2 + 1
-idx = [[] for i in range(n)]
-
+'''
+center = n//2
 idx_up = idx_down = center
-idx_list = []
-idx_list.append(center)
-contnum = 0
+ans = 0
 
 for i in range(n):
-    idx[i] = idx_list
-    contnum += 1
+    for j in range(idx_down, idx_up +1):
+        ans += A[i][j]
 
-    if contnum <= center:
-        idx_up = idx_up + 1
-        idx_down = idx_down -1
+    if i < center:
+            idx_down = idx_down - 1
+            idx_up  = idx_up + 1
     else:
-        idx_up = idx_up -1
-        idx_down = idx_down +1
-    idx_list.append(idx_up)
-    idx_list.append(idx_down)
-print(idx)
+            idx_down  = idx_down +1
+            idx_up = idx_up - 1
+
+print(ans)
