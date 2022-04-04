8ab110de731e717cc2628f055da64518e2929c6f
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-04 16:45:20 +0900

    unsolved

--- 섹션 4/2. 랜선자르기/AA.py
+++ 섹션 4/2. 랜선자르기/AA.py
@@ -1,2 +1,25 @@
-#랜선 자르
-#unsolved
+#랜선 자르기
+
+
+n, k = map(int,input().split())
+A = []
+for i in range(n):
+    tmp=int(input())
+    A.append(tmp)
+'''
+n = 4
+k = 11
+A = [802,743,457,539]
+'''
+max = 0
+iter = min(A)
+for i in range(1,iter):
+    divid_list = []
+    for each in A:
+        divid = each // i
+        divid_list.append(divid)
+    if sum(divid_list) >= 11:
+        if i >= max:
+            max = i
+
+print(max)
