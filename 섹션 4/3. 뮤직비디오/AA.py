26d2d6b8062eb9f92623aeb1eb88e7149318dfde
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-12 21:10:00 +0900

    unsolved

--- 섹션 4/3. 뮤직비디오/AA.py
+++ 섹션 4/3. 뮤직비디오/AA.py
@@ -1,4 +1,16 @@
 #뮤직비디오(결정 알고리즘)
 #unsolved
-#바쁘니까 일단 pass
-#하루만 더
+'''
+n, m = map(int,input().split())
+A = list(map(int, input().split()))
+'''
+
+n = 9
+m = 3
+A = [1,2,3,4,5,6,7,8,9]
+
+def checker(input, A):
+    ret = False
+    if sum(A) < input:
+        ret = True
+    return ret
