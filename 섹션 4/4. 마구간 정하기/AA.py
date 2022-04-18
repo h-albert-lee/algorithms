2727c8130f597a8970233eb6e70e3cce4a6d1c14
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-18 14:09:41 +0900

    unsolved

---
+++ 섹션 4/4. 마구간 정하기/AA.py
@@ -0,0 +1,15 @@
+#마구간 정하기
+#unsolved 
+'''
+n, c = map(int, input().split())
+A = []
+for _ in range(n):
+    a = int(input())
+    A.append(a)
+'''
+n = 5
+c = 3
+A = [1,2,8,4,9]
+A.sort()
+
+def checker(dis, list):
