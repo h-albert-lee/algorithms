7dc5d47f142c068521dde7f569e5a42e40e6a8e8
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-19 22:54:02 +0900

    solved(sol)

--- 섹션 4/4. 마구간 정하기/AA.py
+++ 섹션 4/4. 마구간 정하기/AA.py
@@ -1,6 +1,6 @@
 #마구간 정하기
-#unsolved 
-'''
+#solved(sol)
+
 n, c = map(int, input().split())
 A = []
 for _ in range(n):
@@ -10,6 +10,32 @@ for _ in range(n):
 n = 5
 c = 3
 A = [1,2,8,4,9]
-A.sort()
+'''
 
 def checker(dis, list):
+    cnt = 1
+    stat = list[0]
+    for i in range(1,n):
+        if list[i] - stat >= dis:
+            cnt += 1
+            stat = list[i]
+    return cnt
+
+A.sort()
+
+lt = 1
+rt = max(A)
+
+
+ans = 0
+
+while lt <= rt:
+    mid = (lt+rt) // 2
+    if checker(mid, A) >= c:
+         ans = mid
+         lt = mid +1
+    else:
+        rt = mid -1
+
+
+print(ans)
