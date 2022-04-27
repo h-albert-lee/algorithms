1df7d318bc14827486419ea9bac54bd76c76bca4
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-27 17:57:29 +0900

    solved(sol)

--- 섹션 5/3. 후위표기식 만들기/AA.py
+++ 섹션 5/3. 후위표기식 만들기/AA.py
@@ -1,12 +1,29 @@
 #후위표기식 만들기
-import re
-#s = str(input())
-s = '3+5*2/(7-2)'
+#solved(sol)
+s = str(input())
+#s = '3+5*2/(7-2)'
 stack = []
+ans = ''
 
 
 for i in range(len(s)):
     if s[i].isdecimal():
-        stack.append(s[i])
-    if s[i] == '+' or s[i] == '-':
-        if s[i+1].isdecimal()
+        ans += s[i]
+    else:
+        if s[i] == '(':
+            stack.append(s[i])
+        elif s[i] == '*' or s[i] == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                ans += stack.pop()
+            stack.append(s[i])
+        elif s[i] == '+' or s[i] == '-':
+            while stack and (stack[-1] != '('):
+                ans += stack.pop()
+            stack.append(s[i])
+        elif s[i] == ')':
+            while stack and stack[-1] != '(':
+                ans += stack.pop()
+            stack.pop()
+while stack:
+    ans += stack.pop()
+print(ans)
