c22fbf7bfbaeb975d1a005b57d46ad89f5b171c3
h-albert-lee <gksdnf424@korea.ac.kr>
2022-04-28 09:26:38 +0900

    solved(time)

---
+++ 섹션 5/6. 응급실/AA.py
@@ -0,0 +1,25 @@
+#응급실
+from collections import deque
+
+
+n, m = map(int, input().split())
+A = list(map(int,input().split()))
+'''
+n = 5
+m = 2
+A = [60,50,70,80,90]
+'''
+A = deque(A)
+cnt = 0
+ans = A[m]
+
+while A:
+    k = A.popleft()
+    if k > max(A):
+        cnt += 1
+        if k == ans:
+            break
+    else:
+        A.append(k)
+
+print(cnt)
