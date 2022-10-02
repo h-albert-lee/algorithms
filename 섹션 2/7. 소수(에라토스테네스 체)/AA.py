79ee320aefccae785fe8b0e6b03fa73f46bd6d8d
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-02 15:02:56 +0900

    success

---
+++ 섹션 2/7. 소수(에라토스테네스 체)/AA.py
@@ -0,0 +1,21 @@
+#에라토스테네스 체
+
+N = int(input())
+
+
+#N = 20
+
+lst = list(range(2, N+1))
+sosu = []
+
+while True:
+    a = lst.pop(0)
+    sosu.append(a)
+    iter_num = 20//a
+    for i in range(2,iter_num+1):
+        if a*i in lst:
+            lst.remove(a*i)
+    if len(lst) == 0:
+        break
+
+print(len(sosu))
