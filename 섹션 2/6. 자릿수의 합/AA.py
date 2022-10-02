a0c13acd9bf8aa4633e7aa4e30f6973ad5c04466
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-02 14:45:39 +0900

    success

---
+++ 섹션 2/6. 자릿수의 합/AA.py
@@ -0,0 +1,23 @@
+#자릿수의 합
+
+n = int(input())
+lst = list(map(int, input().split()))
+'''
+n = 3
+lst = [125, 15232, 97]
+'''
+def digit_sum(x):
+    sum = 0
+    x = str(x)
+    for each in x:
+        sum += int(each)
+    return sum
+
+max = 0
+max_num = 0
+for i in lst:
+    if digit_sum(i) > max:
+        max = digit_sum(i)
+        max_num = i
+
+print(max_num)
