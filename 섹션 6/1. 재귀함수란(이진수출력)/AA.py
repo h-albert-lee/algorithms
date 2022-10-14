0cbf0ebc18e3a5ac36bc092d2fa4ad85615fb280
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-14 21:22:39 +0900

    success

--- 섹션 6/1. 재귀함수란(이진수출력)/AA.py
+++ 섹션 6/1. 재귀함수란(이진수출력)/AA.py
@@ -1,12 +1,18 @@
 #재귀함수를 이용한 이진수 출력
 #solved
-def DFS(x):
-    if x == 0:
+
+def DFS(n):
+    if n == 0:
         return
     else:
-        DFS(x//2)
-        print(x%2, end='')
+        DFS(n//2)
+        print(n%2, end = '')
+
+
+
+
+
 
 if __name__=="__main__":
-    k = int(input())
-    DFS(k)
+    n = int(input())
+    DFS(n)
