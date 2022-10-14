e476e4796861ab679acd591aef027d7239b5446c
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-14 15:32:30 +0900

    success

---
+++ 섹션 5/8. 단어찾기/AA.py
@@ -0,0 +1,16 @@
+#단어찾기
+#success
+
+n = int(input())
+words = dict()
+for i in range(n):
+    k = input()
+    words[k] = 0
+for i in range(n-1):
+    k = input()
+    words[k] = 1
+
+for i, v in words.items():
+    if v == 0:
+        print(i)
+        break
