615d8c7fa16242d0c4b40fac378106abc9820175
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-14 13:20:24 +0900

    success

---
+++ 섹션 5/6. 응급실/AA.py
@@ -0,0 +1,33 @@
+#응급실
+
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+'''
+n = 6
+m = 0
+A = [60, 60, 90, 60, 60, 60, 60]
+'''
+B = []
+for i in range(n):
+    l = (A[i], i)
+    B.append(l)
+
+cnt = 0
+
+def return_max(A):
+    max = 0
+    for a, e in A:
+        if a > max:
+            max = a
+    return max
+
+while True:
+    a = B.pop(0)
+    if a[0] >= return_max(B):
+        cnt +=1
+        if a[1] == m:
+            break
+    else:
+        B.append(a)
+
+print(cnt)
