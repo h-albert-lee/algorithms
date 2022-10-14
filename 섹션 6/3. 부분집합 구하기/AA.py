240b758eaa0cc39511276c6a8ef0c95dbfe3f13f
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-14 21:33:22 +0900

    yet

--- 섹션 6/3. 부분집합 구하기/AA.py
+++ 섹션 6/3. 부분집합 구하기/AA.py
@@ -1,12 +1,31 @@
-#부분집합 구하기 (DFS)
-#unsolved
+#이진트리 순회
+'''
+#이진트리순회
 
 def DFS(n):
+    if n > 7:
+        return
+    else:
+        print(n, end = '')
+        DFS(n*2)
+        DFS(n*2+1)
 
 
 
 
+if __name__=="__main__":
+    DFS(1)
+'''
 
-if __name__ : "__main__":
+#부분집합구하기
+
+def DFS(n):
+    if n>3:
+        return
+
+
+
+if __name__=="__main__":
     n = 3
-    
+    DFS(1)
+    A = [0]*n
