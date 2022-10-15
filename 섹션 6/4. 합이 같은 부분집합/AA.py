ff8a5498fc7663cbcae60f58956fb7b86c56128c
h-albert-lee <gksdnf424@korea.ac.kr>
2022-10-15 11:05:46 +0900

    success

--- 섹션 6/4. 합이 같은 부분집합/AA.py
+++ 섹션 6/4. 합이 같은 부분집합/AA.py
@@ -1,14 +1,18 @@
 #합이 같은 부분집합
+import sys
 
+def DFS(L, sum):
+    if L == n:
+        if sum == (total-sum):
+            print("YES")
+            sys.exit(0)
+    else:
+        DFS(L+1, sum + a[L])
+        DFS(L+1, sum)
 
-
-
-
-
-
-
-
-
-if __name__: "__main__":
-    n = 3
-    
+if __name__== "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    DFS(0, 0)
+    print('NO')
